d05b10f0200f505d84db4b8bf225074c6480358c
Viktor Komarov <victorkomarov98@mail.ru>
2021-01-06 12:11:47 +0300

    end treao

--- bintree/treap/treap.go
+++ bintree/treap/treap.go
@@ -1,9 +1,27 @@
 package treap
 
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 type Node struct {
 	Key, Priority int
 	Value         interface{}
 	Left, Right   *Node
+}
+
+//nolint:gosec
+func NewNode(key int, value interface{}) *Node {
+	return &Node{
+		Key:      key,
+		Value:    value,
+		Priority: rand.Int(),
+	}
 }
 
 func split(root *Node, key int) (*Node, *Node) {
@@ -41,4 +59,35 @@ func merge(less, more *Node) *Node {
 	more.Left = merge(less, more.Left)
 
 	return more
+}
+
+func Search(node *Node, key int) *Node {
+	if node == nil {
+		return nil
+	}
+
+	if node.Key == key {
+		return node
+	}
+
+	if node.Key > key {
+		return Search(node.Left, key)
+	}
+
+	return Search(node.Right, key)
+}
+
+func Insert(root *Node, inserted *Node) *Node {
+	less, other := split(root, inserted.Key)
+
+	return merge(merge(less, inserted), other)
+}
+
+func Remove(root *Node, key int) *Node {
+	less, other := split(root, key)
+	if other != nil && other.Key == key {
+		other = merge(other.Left, other.Right)
+	}
+
+	return merge(less, other)
 }
