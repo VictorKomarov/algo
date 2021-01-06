3c0d9288eae509af5c808a20761fc74cc4366d5b
Viktor Komarov <victorkomarov98@mail.ru>
2021-01-06 16:23:25 +0300

    add test template

--- hash/chain/hash_test.go
+++ hash/chain/hash_test.go
@@ -1,18 +1,53 @@
 package chain
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestHashSet(t *testing.T) {
+	type pair struct {
+		key int
+		val interface{}
+	}
+
 	testCases := []struct {
-		desc string
+		desc               string
+		inserted           []pair
+		expectedSize       int
+		expectedLoadFactor int
+		expected           [][]pair
 	}{
 		{
 			desc: "",
 		},
 	}
 	for _, tC := range testCases {
+		tC := tC
 		t.Run(tC.desc, func(t *testing.T) {
+			t.Parallel()
 
+			h := New()
+
+			for _, pair := range tC.inserted {
+				h.Set(pair.key, pair.val)
+			}
+
+			require.Equal(t, tC.expectedSize, h.size, tC.desc)
+			require.Equal(t, tC.expectedLoadFactor, h.loadFactor, tC.desc)
+
+			for i := range h.buckets {
+				head := h.buckets[i]
+				actual := make([]pair, 0)
+
+				for head != nil {
+					actual = append(actual, pair{key: head.key, val: head.value})
+
+				}
+
+				require.Equal(t, tC.expected[i], actual, tC.desc)
+			}
 		})
 	}
 }
