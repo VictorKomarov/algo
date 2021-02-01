9bd713a21140cfe538c299c4b2d4681342e74882
Viktor Komarov <victorkomarov98@mail.ru>
2021-02-01 20:24:58 +0300

    first approach

---
+++ strings/alphabet.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdlib.h>
+
+void init_alphabet_shift(const char *str, size_t len);
+size_t get_shift(char c);
---
+++ strings/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "alphabet.h"
+
+int boyer_moore(const char *str, const char *template)
+{
+    size_t str_len = strlen(str);
+    size_t template_len = strlen(template);
+    if (template_len == 0 || template_len > str_len) return -1;
+
+    init_alphabet_shift(template, template_len);
+
+    size_t begin = 0;
+    while (begin + template_len -1 < str_len)
+    {
+        size_t match = template_len - 1;
+        while (match >= 0 && template[match] == str[begin+match])
+        {
+            match--;
+        }
+
+        if (match == -1) 
+        {
+            return begin;
+        }
+        
+        begin += get_shift(str[begin]);
+    }
+    
+    return -1;
+}
+
+int main()
+{
+    printf("%d\n", boyer_moore("kololkol", "kol"));
+    return EXIT_SUCCESS;
+}
